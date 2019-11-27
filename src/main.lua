c550997b7f6d34672a1f229aae872d574afafb59
gvtret <tretelnitsky.george@yandex.ru>
2019-11-27 16:17:42 +0300

    Socket support

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,7 @@
 package.path = package.path..';./src/?.lua'..';./src/xml2lua/?.lua'..';./src/u-test/?.lua'..';./src/utf8/?.lua'
 
 if DEBUG then
-  require('debugger')()
+    require('debugger')()
 end
 
 require ('Application')
@@ -10,21 +10,21 @@ require ('TemplateEngine')
 require ('MmiData')
 
 local socket = require('socket')
-local connection = nil
+local connection
 local connected = false
 while true do
-  print("Try connect to 127.0.0.1:9090...")
-  local c, err = socket.connect('127.0.0.1', 9090)
-  if err ~= nil then
-    print(err)
-    print("Wait for 5 secs...")
-    socket.sleep(5)
-  else
-    print("Connected to: "..c:getpeername()) 
-    connection = c
-    break
-  end
-end  
+    print("Try connect to 127.0.0.1:9090...")
+    local c, err = socket.connect('127.0.0.1', 9090)
+    if err ~= nil then
+        print(err)
+        print("Wait for 5 secs...")
+        socket.sleep(5)
+    else
+        print("Connected to: "..c:getpeername())
+        connection = c
+        break
+    end
+end
 
 local eventmap_name = 'src/Events.xml'
 local templates_name = 'src/Templates.xml'
@@ -36,5 +36,5 @@ local ok, err = pcall(app.loop())
 app.stop()
 
 if not ok then
-  print("Error: "..err)
+    print("Error: "..err)
 end
